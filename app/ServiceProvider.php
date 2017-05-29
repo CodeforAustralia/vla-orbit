07865dfb0c5b07068ce4a46353ac372e03806d94
Christian Arevalo <carevaloq87@hotmail.com>
2017-05-29 20:00:18 +1000

    remove ws_init() from model, now is done by repositories vla soap class

--- app/ServiceProvider.php
+++ app/ServiceProvider.php
@@ -95,14 +95,5 @@ Class ServiceProvider
         }
 
     }
-    
-    public function ws_init() 
-    {       
-        // Create Soap Object
-        $wsdl = env( 'ORBIT_WDSL_URL' );
-        $client = new \SoapClient( $wsdl, array( 'cache_wsdl' => WSDL_CACHE_NONE ) );
-
-        return $client;
-    }    
 }
 
