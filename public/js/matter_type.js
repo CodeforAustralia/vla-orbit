b683ee27796bde98928c48ab7f27eba4c04f3f7c
Christian Arevalo <carevaloq87@hotmail.com>
2017-05-31 17:28:39 +1000

    Data table hander for catchment added and edit buttons for service and service providers

--- public/js/matter_type.js
+++ public/js/matter_type.js
@@ -216,12 +216,14 @@ var TableDatatablesAjax = function () {
                         { data: "Phone" },
                         { data: "Email" },
                         { data: "Wait" },
+                        { data: "ServiceTypeName" },
+                        { data: "ServiceLevelName" },
                         {
                             data: null,
                             className: "center",
                             render: function ( data, type, row ) {
                                 // Combine the first and last names into a single table field
-                                return getButtons('service', data.ServiceProviderId) ;
+                                return getButtons('service', data.ServiceId) ;
                             }
                         }
                 ],
@@ -276,7 +278,7 @@ var TableDatatablesAjax = function () {
                             className: "center",
                             render: function ( data, type, row ) {
                                 // Combine the first and last names into a single table field
-                                return getButtons('service_level', data.ServiceProviderId) ;
+                                return getButtons('service_level', data.ServiceLevelId) ;
                             }
                         }
                 ],
@@ -331,7 +333,7 @@ var TableDatatablesAjax = function () {
                             className: "center",
                             render: function ( data, type, row ) {
                                 // Combine the first and last names into a single table field
-                                return getButtons('service_type', data.ServiceProviderId) ;
+                                return getButtons('service_type', data.ServiceTypelId) ;
                             }
                         }
                 ],
@@ -341,12 +343,71 @@ var TableDatatablesAjax = function () {
     }
 
 
+    var handleCatchment = function () {
+
+        var grid = new Datatable();
+
+        grid.init({
+            src: $("#datatable_ajax_catchment"),
+            onSuccess: function (grid, response) {
+                // grid:        grid object
+                // response:    json object of server side ajax response
+                // execute some code after table records loaded
+                console.log(response);
+            },
+            onError: function (grid) {
+                // execute some code on network or other general error  
+            },
+            onDataLoad: function(grid) {
+                // execute some code on ajax data load
+            },
+
+            dataTable: { // here you can define a typical datatable settings from http://datatables.net/usage/options 
+
+                // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+                // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/scripts/datatable.js). 
+                // So when dropdowns used the scrollable div should be removed. 
+                //"dom": "<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'<'table-group-actions pull-right'>>r>t<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'>>",
+               
+                "dom": "<'row'<'col-md-8 col-sm-12'i><'col-md-4 col-sm-12'<'table-group-actions pull-right'>>r>t<'row'<'col-md-8 col-sm-12'i><'col-md-4 col-sm-12'>>",
+
+                "ajax": {
+                    "url": "/catchment/list", // ajax source
+                    "type": "get"
+                },
+                "order": [
+                    [1, "asc"]
+                ],// set first column as a default sort by asc,
+                
+                "bInfo": false,
+                "columns": [
+                        //{ data: "CatchmentId" },
+                        { data: "PostCode" },
+                        { data: "Suburb" },
+                        { data: "LGC" },
+                        /*
+                        {
+                            data: null,
+                            className: "center",
+                            render: function ( data, type, row ) {
+                                // Combine the first and last names into a single table field
+                                return getButtons('catchment', data.ServiceProviderId) ;
+                            }
+                        }*/
+                ],
+
+            }
+        });
+    }
 
     var getButtons = function (controller, id) {
 
         var delete_btn = '<a href="/' + controller + '/delete/' + id  +  '" class="badge badge-danger">Delete</a>';
         var edit_btn = '';
-        //var edit_btn = '<a href="/' + controller + '/show/' + id  +  '" class="badge badge-warning">Edit</a>';
+        if (controller === 'service_provider' || controller === 'service') 
+        {
+            var edit_btn = '<a href="/' + controller + '/show/' + id  +  '" class="badge badge-warning">Edit</a>';
+        }
         return edit_btn + delete_btn;   	
 
     }
@@ -360,6 +421,7 @@ var TableDatatablesAjax = function () {
             handleService();
             handleServiceLevel();
             handleServiceType();
+            handleCatchment();
         }
 
     };
