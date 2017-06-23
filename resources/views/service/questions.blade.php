f07c321617c0982eb72a16ac83326540fb928743
terminals <mike.calle@gmail.com>
2017-06-23 13:16:28 +1000

    Update questions.blade.php

--- resources/views/service/questions.blade.php
+++ resources/views/service/questions.blade.php
@@ -1,11 +1,12 @@
-    <br>
+
     <div class="col-xs-12">
-        <h4>>Legal Matter Conditions</h4>
-    </div>
-    <div class="form-group">
-        <label class="col-md-12 tooltips" data-container="body" data-placement="right" data-original-title="If you have updated your legal matters please Save first">Total Legal Matters ({{ sizeof($current_service->ServiceMatters) }})</label>
-        <div class="col-md-12">
-            <a class=" btn dark btn-outline sbold tooltips" data-toggle="modal" href="#long"  data-container="body" data-placement="right" data-original-title="If you have updated your legal matters please Save first"> Add Condition </a>
+        <h4>Legal Matter Conditions</h4>
+        <p>Add a condition to a legal matter or override the eligibility criteria for a specific Legal Matter in this service</p>
+        <div class="form-group">
+            <label class="col-md-12 tooltips" data-container="body" data-placement="right" data-original-title="If you have updated your legal matters please Save first">Total Legal Matters ({{ sizeof($current_service->ServiceMatters) }})</label>
+            <div class="col-md-12">
+                <a class=" btn dark btn-outline sbold tooltips" data-toggle="modal" href="#long"  data-container="body" data-placement="right" data-original-title="If you have updated your legal matters please Save first"> Add Condition </a>
+            </div>
         </div>
     </div>
 
