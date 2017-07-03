70356a312347c7464dc139411e22e6d0eab11b6d
Christian Arevalo <carevaloq87@hotmail.com>
2017-07-03 11:23:45 +1000

    Add Booking wsdl to repository

--- app/Repositories/VlaSoap.php
+++ app/Repositories/VlaSoap.php
@@ -11,5 +11,14 @@ class VlaSoap
         $client = new \SoapClient( $wsdl, array( 'cache_wsdl' => WSDL_CACHE_NONE ) );
 
         return $client;
+    }
+
+    public function ws_booking_init() 
+    {       
+        // Create Soap Object
+        $wsdl = env( 'ORBIT_BOOKING_WDSL_URL' );
+        $client = new \SoapClient( $wsdl, array( 'cache_wsdl' => WSDL_CACHE_NONE ) );
+
+        return $client;
     }  
 }
