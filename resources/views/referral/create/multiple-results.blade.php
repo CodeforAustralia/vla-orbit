ddbb71bde4e98a497789c7c094c15b11cc093817
terminals <mike.calle@gmail.com>
2017-07-31 18:26:14 +1000

    colour fixes to step nav

--- resources/views/referral/create/multiple-results.blade.php
+++ resources/views/referral/create/multiple-results.blade.php
@@ -12,21 +12,21 @@
     </div>
     <div class="col-xs-2 mt-step-col done" style="cursor: pointer;" onclick="window.legal_issue='/referral/create/legal_issue';">
       <div class="mt-step-number bg-white">
-        <i class="fa fa-legal font-grey-cascade"></i>
+        <i class="fa fa-legal"></i>
       </div>
       <div class="mt-step-title font-grey-cascade">Legal Issue</div>
       <div class="mt-step-content font-grey-cascade"></div>
     </div>
     <div class="col-xs-2 mt-step-col done">
       <div class="mt-step-number bg-white">
-        <i class="fa fa-check-square-o font-grey-cascade"></i>
+        <i class="fa fa-check-square-o"></i>
       </div>
       <div class="mt-step-title font-grey-cascade">Client Details</div>
       <div class="mt-step-content font-grey-cascade"></div>
     </div>
     <div class="col-xs-2 mt-step-col done">
       <div class="mt-step-number bg-white">
-        <i class="fa fa-question font-grey-cascade"></i>
+        <i class="fa fa-question"></i>
       </div>
       <div class="mt-step-title font-grey-cascade">Questions</div>
       <div class="mt-step-content font-grey-cascade"></div>
