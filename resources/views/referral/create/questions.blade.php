2b7911104dbff3342ce29f357c9b37301281bd59
terminals <mike.calle@gmail.com>
2017-07-31 18:33:15 +1000

    Update header

--- resources/views/referral/create/questions.blade.php
+++ resources/views/referral/create/questions.blade.php
@@ -48,7 +48,7 @@
     <div class="portlet light">
       <div class="portlet-title">
         <div class="caption">
-          <span class="caption-subject font-green sbold"><i class="icon-question font-green"></i>&nbsp; Matching Questions</span>
+          <span class="caption-subject font-green sbold"><i class="icon-question font-green"></i>&nbsp; Questions</span>
         </div>
         <div class="pull-right caption">
             <small>{{ $service_qty }} Services Matched</small>       
