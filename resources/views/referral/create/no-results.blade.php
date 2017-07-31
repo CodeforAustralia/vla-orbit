96faf831e9b3ad04236fc86d1daafca28b115fd5
terminals <mike.calle@gmail.com>
2017-07-31 17:53:40 +1000

    Fix to link

--- resources/views/referral/create/no-results.blade.php
+++ resources/views/referral/create/no-results.blade.php
@@ -5,7 +5,7 @@
   <div class="col-xs-10 col-xs-offset-1">
 		<div class="note note-success">
 			<h4 class="block">No matches found.</h4>
-			<p>We were unable to find any free legal services. Please see our alternative referral options <a href="http://vla.vic.gov.au>here</a>.</p>
+			<p>We were unable to find any free legal services. Please see our alternative referral options <a href="http://vla.vic.gov.au">here</a>.</p>
 			<br>				
 			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right" > Create New Referral </a>
 		</div>
