f110336fb20f505e0799107f456eca77c9c2c227
terminals <mike.calle@gmail.com>
2017-07-31 11:25:56 +1000

    Fix header
    Removed row tag on heading div - added gutter to h1 and New Booking button

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -5,7 +5,7 @@
     @include ('orbit.alert')
     
     <!-- BEGIN PAGE HEADER-->
-    <div class="portlet row">
+    <div class="portlet">
         <h1 class="page-title col-xs-10"> All Bookings </h1>
         <a role="button" class="btn main-green col-xs-2 pull-right" href="/booking/new">New Booking</a>
         <br>
