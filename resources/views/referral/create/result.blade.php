000a72aebac9185741161fca5f255a81b69dc642
terminals <mike.calle@gmail.com>
2017-07-31 17:26:10 +1000

    Content fixes

--- resources/views/referral/create/result.blade.php
+++ resources/views/referral/create/result.blade.php
@@ -32,7 +32,7 @@
                 <div class="col-xs-6 col-sm-8">
                   <h3 class="service-name"><strong>Homeless Law Service</strong></h3>
                   <h4 class="service-provider-name">Justice Connect</h4>
-                  <p>Send the client the contact details of this service and a record of this Orbit referral by Email, SMS or both with the form below.</p>
+                  <p>Send referral details to the client by Email, SMS or both with the form below.</p>
                 </div>
               </div>
 
@@ -82,14 +82,16 @@
                 </div>
               </div>
             </div>
-          </div> <!-- Modal Container -->
+          </div> 
+          
+          <!-- Modal Container -->
             
             <div class="row" id="result-step-2" style="display:none">
               <div class="col-xs-12 text-center">
                 <p style="font-size: 126px;"><i class="fa fa-check-circle" style="color: #5cb85c;background-color: #fff;"></i></p>
-                <h3><strong>Referral Sent to Client</strong></h3>
+                <h3><strong>Referral sent to client</strong></h3>
                 <h3><strong>ID: #</strong><span id="referral_id"></span></h3><br>
-                <button type="button" class="btn default btn-outline btn-lg" data-toggle="modal" data-target="#SelectMatch" id="close-modal"><span>View Results</span></button>
+                <button type="button" class="btn default btn-outline btn-lg" data-toggle="modal" data-target="#SelectMatch" id="close-modal"><span>Return to Matches</span></button>
                 <button type="button" class="btn green-jungle btn-lg" onClick="window.location='/referral';">Return to Dashboard</button><br><br><br><br>
               </div>
             </div>
