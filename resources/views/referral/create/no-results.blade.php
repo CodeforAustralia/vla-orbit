9ae4123beb51b059c036cc83cb252a90b938b29f
terminals <mike.calle@gmail.com>
2017-07-31 17:38:48 +1000

    Updated content and changed button copy

--- resources/views/referral/create/no-results.blade.php
+++ resources/views/referral/create/no-results.blade.php
@@ -5,10 +5,9 @@
   <div class="col-xs-10 col-xs-offset-1">
 		<div class="note note-success">
 			<h4 class="block">No matches found.</h4>
-			<p> This client is not elegible for any service, please check the non legal issues page. <a href="#">Link</a> </p>
-			<br>
-				
-			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right" > Start again </a>
+			<p>We were unable to find any free legal services. Please see our alternative referral options <a href="http://vla.vic.gov.au>here</a>.</p>
+			<br>				
+			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right" > Create New Referral </a>
 		</div>
 
   </div> <!-- Col Close -->
