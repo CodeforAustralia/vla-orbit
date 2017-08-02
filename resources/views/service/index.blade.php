5f56f14a2606758a1efcbe7471e3ac5e44a79719
Christian Arevalo <carevaloq87@hotmail.com>
2017-08-02 13:32:04 +1000

    Remove s from new services

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -7,7 +7,7 @@
     <!-- BEGIN PAGE HEADER-->
     <div class="portlet ">
         <h1 class="page-title col-xs-10"> All Services </h1>
-        <a role="button" class="btn main-green col-xs-2 pull-right" href="/service/new">New Services</a>
+        <a role="button" class="btn main-green col-xs-2 pull-right" href="/service/new">New Service</a>
         <br>
     </div>
     <!-- END PAGE HEADER-->
