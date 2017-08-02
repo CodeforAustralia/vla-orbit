6ea20c50b369152afdc916f973a0e15e505fe45d
Christian Arevalo <carevaloq87@hotmail.com>
2017-08-02 14:55:55 +1000

    Remove one video thumbnail and add wall of messages

--- resources/views/orbit/index.blade.php
+++ resources/views/orbit/index.blade.php
@@ -62,44 +62,69 @@
     <div class="clearfix"></div>
     <!-- END DASHBOARD STATS 1-->
     
+    <div class="row">
+        <!-- BEGIN THUMBNAILS PORTLET-->
+        <div class="col-sm-12 col-md-6">
+            <div class="portlet light ">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="fa fa-book font-green-sharp"></i>
+                        <span class="caption-subject font-green-sharp bold">REFERRAL SCHOOL</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="thumbnail">
+                                <img src="/assets/layouts/layout2/img/overlay.jpg" alt="">
+                                <div class="caption">
+                                    <h3>How to create Services</h3>
+                                    <p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
+                                    <p>
+                                        <a href="javascript:;" class="btn red"> Read More </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END THUMBNAILS PORTLET-->
+                       
+        <!-- BEGIN WELL PORTLET-->
 
-    <!-- BEGIN THUMBNAILS PORTLET-->
-    <div class="portlet light ">
-        <div class="portlet-title">
-            <div class="caption">
-                <i class="fa fa-book font-green-sharp"></i>
-                <span class="caption-subject font-green-sharp bold">Referral School</span>
-            </div>
-        </div>
-        <div class="portlet-body">
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <div class="thumbnail">
-                        <img src="/assets/layouts/layout2/img/overlay.jpg" alt="">
-                        <div class="caption">
-                            <h3>How to create Services</h3>
-                            <p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
-                            <p>
-                                <a href="javascript:;" class="btn red"> Read More </a>
-                            </p>
-                        </div>
+        <div class="col-sm-12 col-md-6">
+
+            <div class="portlet light ">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-share font-red-sunglo"></i>
+                        <span class="caption-subject font-red-sunglo bold uppercase">WAll</span>
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="thumbnail">
-                        <img src="/assets/layouts/layout2/img/overlay.jpg" alt="">
-                        <div class="caption">
-                            <h3>How to do a better referral?</h3>
-                            <p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
-                            <p>
-                                <a href="javascript:;" class="btn red"> Read More </a>
-                            </p>
-                        </div>
+
+                <div class="portlet-body">
+                    <div class="note note-info">
+                        <h4 class="block bold font-grey-mint">Announcements</h4>
+                        <p> Thanks for using Orbit. We are currently testing the service and need to know what is working, what isn't and if something is missing. Please send any feedback to <a href="mailto:orbitgeneric@gmail.com">orbitgeneric@gmail.com</a> or use the chat tool. </p>
+                        <p></p>
+                        <p>- Orbit team</p>
+                    </div>
+                    <div class="note note-info">
+                        <h4 class="block bold font-grey-mint">Keywords</h4>
+                        <p> To help navigate the search for the right legal issue an overview of the taxonomy used in Orbit with related keywords are available <a href="https://docs.google.com/spreadsheets/d/1L-pg_KxX9WP29CsE--3OvgyFE8BjL7CmgPBFHBw4-KM/" target="_blank">here</a>. </p>
+                    </div>
+                    <div class="note note-info">
+                        <h4 class="block bold font-grey-mint">Non-Legal Referrals</h4>
+                        <p> Find a list of alternative referral options <a href="https://docs.google.com/document/d/1meV27i-3VT2o4zqN7bCWDoAa8gia96adz6u0V9qVtgs/" target="_blank">here</a>.</p>
                     </div>
                 </div>
             </div>
+
         </div>
+        <!-- END WELL PORTLET-->
     </div>
-    <!-- END THUMBNAILS PORTLET-->
+    
 
 @endsection
