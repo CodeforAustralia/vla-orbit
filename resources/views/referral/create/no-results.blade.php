91c95b6e51642e0f20bd5109838bb43da968e8b5
Christian Arevalo <carevaloq87@hotmail.com>
2017-08-02 11:23:11 +1000

    Add text and link to no results page

--- resources/views/referral/create/no-results.blade.php
+++ resources/views/referral/create/no-results.blade.php
@@ -5,7 +5,7 @@
   <div class="col-xs-10 col-xs-offset-1">
 		<div class="note note-success">
 			<h4 class="block">No matches found.</h4>
-			<p>We were unable to find any free legal services. Please see our alternative referral options <a href="http://vla.vic.gov.au">here</a>.</p>
+			<p>Sorry, no matching legal services found. For alternative referral options please check the non-legal referral list. <a href="https://docs.google.com/document/d/1meV27i-3VT2o4zqN7bCWDoAa8gia96adz6u0V9qVtgs/edit#" target="_blank">Link</a>.</p>
 			<br>				
 			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right" > Create New Referral </a>
 		</div>
