3876fbbf3ef4e22511e650b587618e4521175f75
Christian Arevalo <carevaloq87@hotmail.com>
2017-08-11 14:39:10 +1000

    Change text

--- resources/views/emails/referral/sms.blade.php
+++ resources/views/emails/referral/sms.blade.php
@@ -1,10 +1,8 @@
-Hi, this is the referral details to contact {{ $args['ServiceProviderName'] }} for their {{ $args['ServiceName'] }} service.\n\n
-
+Hi, here are the details to contact {{ $args['ServiceProviderName'] }} for their {{ $args['ServiceName'] }} service.\n\n
 Contact details:\n
 {{ $args['Phone'] }}  \n
 @if( $args['Location'] != "#" )
 {{ $args['Location'] }} \n
 @endif
 {{ $args['URL'] }}\n\n
-
 Once you make contact they will assess whether they can help you or not.
