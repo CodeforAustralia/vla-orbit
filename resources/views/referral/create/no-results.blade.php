ab783b03e9c8bf31fdf158688408ac4d32e1e6f1
Christian Arevalo <carevaloq87@hotmail.com>
2017-09-11 16:12:56 +1000

    Add sad man image and change layout of no results page on matching services

--- resources/views/referral/create/no-results.blade.php
+++ resources/views/referral/create/no-results.blade.php
@@ -3,13 +3,22 @@
 @section ('content')
 <div class="row">
   <div class="col-xs-10 col-xs-offset-1">
-		<div class="note note-success">
-			<h4 class="block">No matches found.</h4>
-			<p>Sorry, no matching legal services found. For alternative referral options please check the non-legal referral list. <a href="https://docs.google.com/document/d/1meV27i-3VT2o4zqN7bCWDoAa8gia96adz6u0V9qVtgs/edit#" target="_blank">Link</a>.</p>
-			<br>				
-			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right" > Create New Referral </a>
-		</div>
 
+	<br>
+	<br>
+
+	<div class="text-center">
+		<div class="snap-block">
+			<img class="snap-image" src="/assets/global/img/sad_man.png" width="200">
+			<h1 class="bold" >oh snap!</h1>
+		</div>
+		<div class="snap-text">
+			<p class="bold">No matching legal service found. For alternative referral options check out the non-legal referral list <a href="https://docs.google.com/document/d/1meV27i-3VT2o4zqN7bCWDoAa8gia96adz6u0V9qVtgs/edit#" target="_blank" class="font-green-jungle">here</a>. </p>
+			
+			<a href="/referral/create/location" class="btn green-jungle btn-lg push-right"> Create New Referral </a>
+		</div>
+	</div>
+	
   </div> <!-- Col Close -->
 </div> <!-- Row Close -->
 @endsection
