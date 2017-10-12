9623f9255524c0fb6e3fb74af1a95d16917f89c6
Christian Arevalo <carevaloq87@hotmail.com>
2017-10-12 14:12:54 +1100

    Remove title on users page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,9 +1,6 @@
 @extends ('orbit.master')
 
 @section ('content')
-    <!-- BEGIN PAGE HEADER-->
-    <h1 class="page-title"> Specific Matter </h1>
-    <!-- END PAGE HEADER-->
     
     @include ('user.form')
     
