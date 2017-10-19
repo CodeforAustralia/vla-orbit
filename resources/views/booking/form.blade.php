603e7baef359914013e93b2984186a12303c5287
Christian Arevalo <carevaloq87@hotmail.com>
2017-10-19 11:49:24 +1100

    Re organize fields of client information

--- resources/views/booking/form.blade.php
+++ resources/views/booking/form.blade.php
@@ -36,7 +36,7 @@
                         <option> </option>
                     </select>
                 </div>
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-4 hidden">
                     <button type="button" class="btn btn-block dark btn-outline" data-toggle="modal" data-target="#EligibilityConfirm">View Service Details</button>
                 </div>
             </div>
@@ -48,7 +48,7 @@
         <div class="col-xs-12">
             <div class="form-group">
                 <div class="col-xs-8 padding-bottom-10">
-                    <label>Interpreter Service (if required):</label>
+                    <label>Interpreter Service:</label>
                     <select class="form-control" id="Language" name="Language">    
                         @include( 'booking.language' )
                     </select>                    
@@ -66,18 +66,16 @@
     <h4 class="padding-top-10 padding-bottom-10">Client Details</h4>
     
     <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-4">
             <div class="form-group">
-                <div class="col-xs-12 padding-bottom-10">
+                <div class="col-xs-12 padding-bottom-20">
                     <label>First Name:</label>
                     <input type="text" class="form-control input-large" placeholder="Jane" name="client[FirstName]" required> 
                 </div>
             </div>
         </div>
-    </div>
-            
-   <div class="row">
-        <div class="col-xs-12">        
+
+        <div class="col-xs-4">        
             <div class="form-group">
                 <div class="col-xs-12 padding-bottom-20">
                     <label>Last Name:</label>
@@ -87,33 +85,17 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="form-group">
-                <div class="col-xs-12 padding-bottom-10">
-                    <label>CIR Number:</label>
-                    <input type="text" class="form-control input-large" placeholder="1234567" name="CIRNumber" id="CIRNumber">                 
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <hr>
-    <h4 class="padding-top-10 padding-bottom-10">Contact Details</h4>    
-
    <div class="row">
-        <div class="col-xs-12">                            
+        <div class="col-xs-4">                            
             <div class="form-group">
-                <div class="col-xs-12 padding-bottom-10">
+                <div class="col-xs-12 padding-bottom-20">
                     <label>Email:</label>
                     <input type="text" class="form-control input-large" placeholder="janesmith@gmail.com" name="client[ClientEmail]" id="email"> 
                 </div>
             </div>
         </div>
-    </div>
-    
-    <div class="row">
-        <div class="col-xs-12">
+
+        <div class="col-xs-5">
             <div class="form-group">
                 <div class="col-xs-12">
                     <label>Is it safe to contact this client by email? &nbsp; <i class="fa fa-info-circle tooltips" aria-hidden="true" data-container="body" data-placement="right" data-original-title="If it is safe to contact the client via email we will send a booking confirmation to the provided email with all of their booking details and may contact this client about changes to their booking. Select 'No' if it is unsafe to email this client and ensure you provide them with their booking details manually."></i></label>
@@ -130,18 +112,16 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-4">
             <div class="form-group">
-                <div class="col-xs-12 padding-bottom-10">
+                <div class="col-xs-12 padding-bottom-20">
                     <label>Phone Number:</label>
                     <input type="text" class="form-control input-large" placeholder="0400 000 000" name="client[Mobile]" id="mobile"> 
                 </div>
             </div>
         </div>
-    </div>
-   
-    <div class="row">
-        <div class="col-xs-12">                            
+
+        <div class="col-xs-5">                            
             <div class="form-group">
                 <div class="col-xs-12">
                     <label>Is it safe to contact this client by phone call and SMS? &nbsp; <i class="fa fa-info-circle tooltips" aria-hidden="true" data-container="body" data-placement="right" data-original-title="If it is safe to contact the client on their phone we may call or send the client an SMS to remind them of their booking or to notify them of changes to their booking. Select 'No' if it is unsafe to call or SMS this client."></i></label>
@@ -153,6 +133,17 @@
                             <input type="radio" name="phonepermission" id="phonepermission" value="No">No<span></span>
                         </label>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="form-group">
+                <div class="col-xs-12 padding-bottom-20">
+                    <label>CIR Number (if known):</label>
+                    <input type="text" class="form-control input-large" placeholder="1234567" name="CIRNumber" id="CIRNumber">                 
                 </div>
             </div>
         </div>
