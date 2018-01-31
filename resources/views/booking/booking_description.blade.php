eacf4b4db3c37fd8b060c6580f589a0dc6d36f74
Christian Arevalo <carevaloq87@hotmail.com>
2018-01-31 12:52:31 +1100

    Add text and enable click out of modal to exit in booking page

--- resources/views/booking/booking_description.blade.php
+++ resources/views/booking/booking_description.blade.php
@@ -1,8 +1,9 @@
 <!-- Confirm Eligibility Modal -->                                        
-<div class="modal fade in" id="bookingTypeDescription" tabindex="-1" data-backdrop="static" data-keyboard="false" aria-hidden="false" role="dialog" aria-labelledby="bookingTypeDescription">
+<div class="modal fade in" id="bookingTypeDescription" tabindex="-1" aria-hidden="false" aria-labelledby="bookingTypeDescription" role="basic">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
                     <h4 class="bold">Direct bookings</h4>
                     <p>Allows direct bookings into a VLA appointment service.</p>
                     <p>Before making a direct booking, ensure the following:</p>
@@ -12,6 +13,7 @@
                         <li>the client is reminded to bring all relevant paperwork to the appointment.</li>
                     </ul>
                     <hr>
+                    <p>These other forms send an e-referral to the office providing the service, replicating the functions of the Outlook e-referral templates. A copy is bcc'd to the LegalInfoCallBack mailbox.</p>
                     <h4 class="bold">Appointment request</h4>
                     <p>For VLA Practice matters. Use where direct bookings are unavailable, or if in doubt about the appropriateness of a direct booking.</p>
                     <h4 class="bold">For assessment</h4>
@@ -21,11 +23,6 @@
                     <h4 class="bold">Duty lawyer</h4>
                     <p>For VLA Practice matters. Use to send a memo to the duty lawyer.</p>
                     <p>For more information on when to use these e-referral templates, please see your Legal Help manual.</p>
-            </div>
-            <div class="modal-footer">
-                <div class="col-sm-6">
-                    <button type="button" data-dismiss="modal" class="btn btn-block btn-outline dark">Close</button>
-                </div>
             </div>
         </div>
     </div>
