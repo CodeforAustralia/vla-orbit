8b1506b251173356e6795c94878f69bb85ac2b24
Christian Arevalo <carevaloq87@hotmail.com>
2018-02-12 14:49:10 +1100

    Add all active menus on side bar

--- resources/views/orbit/sidebar.blade.php
+++ resources/views/orbit/sidebar.blade.php
@@ -13,20 +13,20 @@
                     <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
                     <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                     <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-compact " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-                        <li class="nav-item start ">
+                        <li class="nav-item start {{ Request::is('referral','referral/*') ? 'active' : null }}">
                             <a href="/referral" class="nav-link nav-toggle">
                                 <i class="icon-paper-plane"></i>
                                 <span class="title">Referrals</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('referral','referral/*') ? 'open' : null }}"></span>
                             </a>
                             <ul class="sub-menu">
-                                <li class="nav-item ">
-                                    <a href="/referral" class="nav-link ">
+                                <li class="nav-item {{ Request::is('referral') ? 'active' : null }}">
+                                    <a href="/referral" class="nav-link">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Inbound</span>
                                     </a>
                                 </li>
-                                <li class="nav-item">
+                                <li class="nav-item {{ Request::is('referral/outbound') ? 'active' : null }}">
                                     <a href="/referral/outbound" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Outbound</span>
@@ -37,26 +37,26 @@
 
                         @if ( in_array( \App\Http\helpers::getRole(), ['Administrator', 'AdminSp' , 'VLA']) )
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('booking','booking/*') ? 'active' : null }}">
                             <a href="/booking" class="nav-link nav-toggle">
                                 <i class="icon-calendar"></i>
                                 <span class="title">Bookings</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('booking','booking/*') ? 'open' : null }}"></span>
                             </a>
                             <ul class="sub-menu">
-                                <li class="nav-item">
+                                <li class="nav-item {{ Request::is('booking') ? 'active' : null }}">
                                     <a href="/booking" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">My Office Bookings</span>
                                     </a>
                                 </li>
-                                <li class="nav-item ">
+                                <li class="nav-item {{ Request::is('booking/by_service_provider') ? 'active' : null }}">
                                     <a href="/booking/by_service_provider" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Other Office Bookings</span>
                                     </a>
                                 </li>
-                                <li class="nav-item ">
+                                <li class="nav-item {{ Request::is('booking/next_bookings') ? 'active' : null }}">
                                     <a href="/booking/next_bookings" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">All Bookings</span>
@@ -65,20 +65,20 @@
                             </ul>
                         </li>        
 
-                        <li class="nav-item start ">
+                        <li class="nav-item {{ Request::is('no_reply_emails','no_reply_emails/*') ? 'active' : null }}">
                             <a href="/no_reply_emails" class="nav-link nav-toggle">
                                 <i class="icon-envelope"></i>
                                 <span class="title">No Reply Emails</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('no_reply_emails','no_reply_emails/*') ? 'open' : null }}"></span>
                             </a>
                             <ul class="sub-menu">
-                                <li class="nav-item">
+                                <li class="nav-item {{ Request::is('no_reply_emails/templates') ? 'active' : null }}">
                                     <a href="/no_reply_emails/templates/" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Templates</span>
                                     </a>
                                 </li>
-                                <li class="nav-item ">
+                                <li class="nav-item {{ Request::is('no_reply_emails') ? 'active' : null }}">
                                     <a href="/no_reply_emails/" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">View emails sent</span>
@@ -89,46 +89,38 @@
 
                         @endif
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('service','service/*') ? 'active' : null }}">
                             <a href="/service" class="nav-link">
                                 <i class="icon-notebook"></i>
                                 <span class="title">Services</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('service','service/*') ? 'open' : null }}"></span>
                             </a>
-                            <ul class="sub-menu">
-                                <li class="nav-item ">
-                                    <a href="/service" class="nav-link ">
-                                        <i class="fa fa-list"></i>
-                                        <span class="title">Services</span>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('service_provider','service_provider/*') ? 'active' : null }}">
                             <a href="/service_provider" class="nav-link">
                                 <i class="fa fa-briefcase"></i>
                                 <span class="title">Service providers</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('service_provider','service_provider/*') ? 'open' : null }}"></span>
                             </a>
                         </li>
 
                         @if ( \App\Http\helpers::getRole() == 'Administrator' )
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('matter','matter/*') ? 'active' : null }}">
                             <a href="/matter" class="nav-link nav-toggle">
                                 <i class="fa fa-legal"></i>
                                 <span class="title">Legal Matter</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('matter','matter/*') ? 'open' : null }}"></span>
                             </a>
                             <ul class="sub-menu">
-                                <li class="nav-item ">
+                                <li class="nav-item {{ Request::is('matter') ? 'active' : null }}">
                                     <a href="/matter" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Legal Matter</span>
                                     </a>
                                 </li>
-                                <li class="nav-item hidden ">
+                                <li class="nav-item hidden {{ Request::is('matter_type') ? 'active' : null }}">
                                     <a href="/matter_type" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Matter Type</span>
@@ -137,20 +129,20 @@
                             </ul>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('legal_matter_questions','eligibility_criteria') ? 'active' : null }}">
                             <a href="javascript:;" class="nav-link nav-toggle">
                                 <i class="fa fa-question"></i>
                                 <span class="title">Eligibility</span>
-                                <span class="arrow"></span>
+                                <span class="arrow {{ Request::is('legal_matter_questions','eligibility_criteria') ? 'open' : null }}"></span>
                             </a>
                             <ul class="sub-menu">
-                                <li class="nav-item start ">
+                                <li class="nav-item {{ Request::is('legal_matter_questions') ? 'active' : null }}">
                                     <a href="/legal_matter_questions" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Legal Matter Conditions</span>
                                     </a>
                                 </li>
-                                <li class="nav-item start ">
+                                <li class="nav-item {{ Request::is('eligibility_criteria') ? 'active' : null }}">
                                     <a href="/eligibility_criteria" class="nav-link ">
                                         <i class="fa fa-list"></i>
                                         <span class="title">Eligibility Criteria</span>
@@ -159,7 +151,7 @@
                             </ul>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('sms_template') ? 'active' : null }}">
                             <a href="/sms_template" class="nav-link">
                                 <i class="fa fa-envelope"></i>
                                 <span class="title">Sms Templates</span>
@@ -167,7 +159,7 @@
                             </a>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('catchment') ? 'active' : null }}">
                             <a href="/catchment" class="nav-link">
                                 <i class="fa fa-map-signs"></i>
                                 <span class="title">Catchment</span>
@@ -175,7 +167,7 @@
                             </a>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('statistic') ? 'active' : null }}">
                             <a href="/statistic" class="nav-link">
                                 <i class="fa fa-bar-chart"></i>
                                 <span class="title">Statistics</span>
@@ -183,7 +175,7 @@
                             </a>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('user') ? 'active' : null }}">
                             <a href="/user" class="nav-link">
                                 <i class="fa fa-users"></i>
                                 <span class="title">Users</span>
@@ -191,7 +183,7 @@
                             </a>
                         </li>
 
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('dashboard') ? 'active' : null }}">
                             <a href="/dashboard" class="nav-link">
                                 <i class="fa fa-desktop"></i>
                                 <span class="title">Dashboard wall</span>
