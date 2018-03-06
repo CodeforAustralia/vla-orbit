d917a174d51b5423c5b968141a2a1f7c708cac46
Christian Arevalo <carevaloq87@hotmail.com>
2018-03-07 10:58:53 +1100

    Error 500 handler added to Exceptions class and view

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (!$this->isHttpException($exception)) 
+        {
+            $exception = new \Symfony\Component\HttpKernel\Exception\HttpException(500);
+        }
+
         return parent::render($request, $exception);
     }
 
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,29 @@
+@extends ('orbit.master')
+
+@section ('content')
+
+	<div class="note note-danger">
+
+	    <h4 class="block"> <strong>Error  <small>{{ $exception->getStatusCode() }}</small></strong> - Something went wrong please contact an admin to receive help with this issue <a href="mailto:orbitteam@vla.vic.gov.au">orbitteam@vla.vic.gov.au</a> </h4>	    
+	</div>
+    
+    @if ( \App\Http\helpers::getRole() == 'Administrator' )
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="panel-title font-purple-soft bold uppercase">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse_error"> Error JSON </a>
+            </h4>
+        </div>
+        <div id="collapse_error" class="panel-collapse collapse">
+            <div class="panel-body">
+                
+				<p> <strong>{{ $exception->getFile() }} in line {{ $exception->getLine() }}</strong> </p>
+				<br>
+				<p>  {{ json_encode($exception->getTrace()) }} </p>
+
+            </div>
+        </div>
+    </div>
+    @endif
+
+@endsection
