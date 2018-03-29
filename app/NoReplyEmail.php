c8ee2df9656f01abd0515ffdf4fab5ed1e4f0282
Sebastian Currea <sebastian.currea@gmail.com>
2018-03-29 18:17:30 +1100

    NRE temp invalidation SP phone number and attachments

--- app/NoReplyEmail.php
+++ app/NoReplyEmail.php
@@ -22,8 +22,9 @@ class NoReplyEmail
 		try 
 		{
 			$user = auth()->user();			
-			$response =  $this->client->GetAllTemplatesasJSON() ;			
-			return json_decode( $response->GetAllTemplatesasJSONResult, true );
+			$response =  $this->client->GetAllTemplates()
+									  ->GetAllTemplatesResult;			
+			return json_decode(json_encode($response->EmailTemplates),true);
 		} 
 		catch (Exception $e) 
 		{
@@ -61,8 +62,8 @@ class NoReplyEmail
 			{				
 				$section = self::getSection();			
 				$info = [ 'Section' => $section ];
-				$response = $this->client->GetTemplatesBySectionasJSON( $info );
-				$templates = json_decode( $response->GetTemplatesBySectionasJSONResult, true );			
+				$response = $this->client->GetTemplatesBySection( $info );
+				$templates = json_decode(json_encode($response->GetTemplatesBySectionResult->EmailTemplates),true);
 
 				foreach ($templates as $template) 
 				{
@@ -133,10 +134,12 @@ class NoReplyEmail
 		   		$suffix .= '<em>If you wish to contact us, please do not reply to this message. Replies to this message will not be read or responded to.</em><br><br>';
 		   		$sp_contact .= 'To contact us:<br><br>';
 		   		$sp_contact .= $sp_name . '<br>';
+		   		/* Temp disabled
 		   		if( $service_provider->ContactPhone != '#')
 		   		{
 		   			$sp_contact .= $service_provider->ContactPhone . '<br>';
 		   		}
+		   		*/
 		   		if( $service_provider->ServiceProviderURL != '#')
 		   		{
 		   			$sp_contact .= $service_provider->ServiceProviderURL . '<br>';
@@ -146,7 +149,7 @@ class NoReplyEmail
 
 	       	$prefix = '<em>This email was sent by ' . $sp_name . ' to ' . $email_data['to'] .  ' </em><br><em>Please do not reply to this email.</em><br><hr><br>';
 
-		   	$suffix .= '<br><p classname = \'orbitprefix\' style=\'background: #f5f8fa; padding-top: 15px;box-sizing: border-box;background-color: #f5f8fa;color: #aeaeae; height: 100%;line-height: 1.4; width: 100%;padding-bottom: 15px;font-size: smaller; text-align: center; margin:0px\'>© 2018 Orbit. All rights reserved.</p><p classname = \'emailprefix\' style=\' background: #f5f8fa; padding-top: 15px;box-sizing: border-box;background-color: #f5f8fa;color: #74787e; height: 100%;line-height: 1.4; width: 100%; margin: 1px;padding-bottom: 15px;font-size: small;\'>Disclaimer: The material in this email is a general guide only. It is not legal advice. The law changes all the time and the general information in this email may not always apply to your own situation. The information in this email has been carefully collected from reliable sources. The sender is not responsible for any mistakes or for any decisions you may make or action you may take based on the information in this email. Some links in this email may connect to websites maintained by third parties. The sender is not responsible for the accuracy or any other aspect of information contained in the third-party websites. This email is intended for the use of the person or organisation it is addressed to and must not be copied, forwarded or shared with anyone without the sender’s consent (agreement). If you are not the intended recipient (the person the email is addressed to), any use, sharing, forwarding or copying of this email and/or any attachments is strictly prohibited. If you received this e-mail by mistake, please let the sender know and please destroy the original email and its contents.</p><br><br>';
+		   	$suffix .= '<br><p classname = "orbitprefix" style="background: #f5f8fa; padding-top: 15px;box-sizing: border-box; color: #aeaeae; font-size: smaller; text-align: center; margin:0px">© 2018 Orbit. All rights reserved.</p><p classname = "emailprefix" style=" background: #f5f8fa; padding: 15px;box-sizing: border-box; color: #74787e;line-height: 1.4; margin: 0px; font-size: small;">Disclaimer: The material in this email is a general guide only. It is not legal advice. The law changes all the time and the general information in this email may not always apply to your own situation. The information in this email has been carefully collected from reliable sources. The sender is not responsible for any mistakes or for any decisions you may make or action you may take based on the information in this email. Some links in this email may connect to websites maintained by third parties. The sender is not responsible for the accuracy or any other aspect of information contained in the third-party websites. This email is intended for the use of the person or organisation it is addressed to and must not be copied, forwarded or shared with anyone without the sender’s consent (agreement). If you are not the intended recipient (the person the email is addressed to), any use, sharing, forwarding or copying of this email and/or any attachments is strictly prohibited. If you received this e-mail by mistake, please let the sender know and please destroy the original email and its contents.</p><br><br>';
 
 
 	       	$email_data['message'] = $prefix . $email_data['message'] . $suffix;
@@ -387,6 +390,7 @@ class NoReplyEmail
     	$output[] = $generalTemplates;
     	return $output;
 
-    }    
+    }
+    
 
 }
