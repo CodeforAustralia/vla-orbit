62ca28b7ef8d66fc1e5f5507e04db834359f9577
Christian Arevalo <carevaloq87@hotmail.com>
2018-04-12 11:27:50 +1000

    Transform 24 hours format to 12 hours format when hovering bookings

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -295,8 +295,8 @@ var AppCalendar = function() {
                     
                     tooltip += booking.ServiceName + '<br>';                    
                     tooltip += booking.ServiceProviderName + '<br>';                    
-                    tooltip += data.start._i.split(' ')[1]  + ' - ';
-                    tooltip += data.end._i.split(' ')[1]  + '<br>';                    
+                    tooltip += tConvert ( data.start._i.split(' ')[1] ) + ' - ';
+                    tooltip += tConvert ( data.end._i.split(' ')[1] ) + '<br>';                    
                     tooltip += '</div>';
 
                     $("body").append(tooltip);
@@ -538,6 +538,19 @@ function deleteBookingDialog()
             e.preventDefault();       
         }
     });  
+}
+
+//Taken from https://stackoverflow.com/questions/13898423/javascript-convert-24-hour-time-of-day-string-to-12-hour-time-with-am-pm-and-no
+function tConvert (time) {
+  // Check correct time format and split into components
+  time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+  if (time.length > 1) { // If time format correct
+    time = time.slice (1);  // Remove full string match value
+    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join (''); // return adjusted time or original string
 }
 
 var selectDefaultSP = function()
