9e045dc4cda17a260d64db24449c9cbcd68af10e
Christian Arevalo <carevaloq87@hotmail.com>
2018-05-29 16:11:09 +1000

    Convert date to 12H format

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -273,7 +273,7 @@ var AppCalendar = function() {
 
                     let booking_date_moment = moment(booking_date).toDate();
                     $("#" + calEvent.data.ServiceId).text(moment(booking_date_moment).format('DD/MM/YYYY') + 
-                        ' at ' + booking_time );
+                        ' at ' + tConvert(booking_time) );
 
                     //sentStatus
                     $("#sentStatus").text(sentDatesStr.replace(/,\s*$/, ''));
@@ -564,7 +564,7 @@ function tConvert (time) {
 
   if (time.length > 1) { // If time format correct
     time = time.slice (1);  // Remove full string match value
-    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+    time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
     time[0] = +time[0] % 12 || 12; // Adjust hours
   }
   return time.join (''); // return adjusted time or original string
