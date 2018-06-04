e1ba3071e81124b6200e36688c26a37f13444f24
Sebastian Currea <sebastian.currea@gmail.com>
2018-06-04 17:24:01 +1000

    In-line documentation roles check

--- app/Repositories/RolesCheck.php
+++ app/Repositories/RolesCheck.php
@@ -3,15 +3,27 @@
 namespace App\Repositories;
 
 use Auth;
-
+/**
+ * Role verification service.
+ * 
+ * @author VLA & Code for Australia
+ * @version 1.0.0
+ */
 class RolesCheck
 {
+	/**
+	 * Check if the user role is administrator
+	 * @return boolean true if is administrator false otherwise.
+	 */
     public function is_admin() 
     {       
         $role = Auth::user()->roles()->first()->name;        
         return ($role == 'Administrator');
     }
-
+    /**
+     * Return the role for the user in session
+     * @return String user role
+     */
     public function getRole()
     {
     	return Auth::user()->roles()->first()->name;
