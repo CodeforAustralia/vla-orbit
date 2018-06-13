963cc2f936814cc76cee0b4986efb6ff787d84a3
Sebastian Currea <sebastian.currea@gmail.com>
2018-06-13 16:00:24 +1000

    Panel Lawyers sms correction

--- resources/views/emails/referral/panel_lawyer_sms.blade.php
+++ resources/views/emails/referral/panel_lawyer_sms.blade.php
@@ -2,10 +2,10 @@
 	$nearest = json_decode($args['Nearest'], true);
 	$nearest = array_slice( $nearest, 0,  5 );
 @endphp
-Hi, here are the details to contact\n\n
+Hi, here are nearby lawyers able to get a grant of legal aid:\n\n
 @foreach( $nearest as $near )
-{{ strtoupper(strtolower($near['office']['OfficeName']) ) }}\n
-{{ $near['office']['FullAddress'] }}\n
-{{ $near['office']['OfficePhone'] or '' }}\n\n
+{!! strtoupper(strtolower($near['office']['OfficeName']) ) !!}\n
+{!! $near['office']['FullAddress'] !!}\n
+{!! $near['office']['OfficePhone'] or '' !!}\n\n
 @endforeach
 Once you make contact they will assess whether they can help you or not.
