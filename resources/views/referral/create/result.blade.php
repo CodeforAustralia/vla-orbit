e723caabe93ee4e1ab324673f39b73be893c45bf
Sebastian Currea <sebastian.currea@gmail.com>
2018-06-13 15:09:46 +1000

    Panel Lawyers minor corrections

--- resources/views/referral/create/result.blade.php
+++ resources/views/referral/create/result.blade.php
@@ -38,7 +38,7 @@
     </div> <!-- Col Close -->
   </div> <!-- Row Close -->
     @include ('referral.create.booking')
-  <!-- Modal Start -->     
+  <!-- Modal Start -->
   <div class="modal fade" id="SelectMatch" tabindex="-1" role="dialog" aria-labelledby="SelectMatchLabel" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
@@ -64,10 +64,10 @@
                 </div>
               </div>
 
-              <!-- Start Form --> 
+              <!-- Start Form -->
               <div class="row">
                 <div class="col-xs-12">
-                  
+
                   <!-- Send to Client form -->
                   <form autocomplete="off">
                     <!-- Email Address -->
@@ -79,20 +79,20 @@
                         <input type="text" name="address" id="address" class="form-control">
                       </div>
                       <div class="col-xs-3 padding-0 margin-bottom-10">
-                        <button onclick="getAddress()" class="form-control padding-0 btn grey" type="button">Update Map</button>
+                        <button onclick="getAddress()" class="form-control padding-0 btn grey" type="button">Search</button>
                       </div>
                       <div class="col-xs-12 padding-0 margin-bottom-10">
-                        <div id="map"></div>                    
+                        <div id="map"></div>
                         <input type="text" name="nearest" id="nearest" class="hidden">
                       </div>
                     </div>
                     <div class="form-group">
                       <label>
-                        <input type="checkbox" id="safeEmail"> It is safe to send an email to this address                        
+                        <input type="checkbox" id="safeEmail"> It is safe to send an email to this address
                         <i class="fa fa-info-circle tooltips" aria-hidden="true" data-container="body" data-placement="right" data-original-title="Please be mindful of any family violence risks; eg if this email account is accessible by others."></i>
-                      </label> 
+                      </label>
                       <label class="sr-only" for="Email">Email Address</label>
-                      <input type="email" class="form-control" id="Email" placeholder="Client email address">                        
+                      <input type="email" class="form-control" id="Email" placeholder="Client email address">
                       <div class="col-xs-11 col-xs-offset-1">
                         <div class="form-group">
                           <div class="checkbox">
@@ -100,7 +100,7 @@
                         </div>
                       </div>
                     </div>
-                    
+
                     <!-- Phone Number -->
                     <div class="form-group">
                       <label>
@@ -113,15 +113,15 @@
                       <div class="col-xs-11 col-xs-offset-1">
                         <div class="form-group">
                           <div class="checkbox">
-                          </div>  
+                          </div>
                         </div>
                       </div>
                     </div>
 
                     <!-- Notes -->
                     <div class="form-group">
-                      <label for="Notes">Referral reason <small>(information is <strong><u>not</u></strong> shared with client)</small></label>                      
-                      <select class="form-control" id="Notes" name="Notes">                                
+                      <label for="Notes">Referral reason <small>(information is <strong><u>not</u></strong> shared with client)</small></label>
+                      <select class="form-control" id="Notes" name="Notes">
                                 <option></option>
                                 <option>Not eligible - Already has solicitor</option>
                                 <option>Not eligible - Catchment</option>
@@ -138,9 +138,9 @@
                                 <option>Other</option>
                         </select>
                     </div>
-                    
+
                     <!-- Extra -->
-                    <div class="form-group">                      
+                    <div class="form-group">
                       <input type="text" class="form-control hidden" id="CatchmentId" name="CatchmentId" value="{{ session('ca_id') }}">
                       <input type="text" class="form-control hidden" id="MatterId" name="MatterId" value="{{ session('mt_id') }}">
                       <input type="text" class="form-control hidden" id="UserID" name="UserID" value="{{ Auth::user()->id }}">
@@ -148,7 +148,7 @@
                     </div>
 
                     <!-- Button -->
-                    <div class="col-xs-8 col-sm-6 col-xs-offset-2 col-sm-offset-3"><br>                        
+                    <div class="col-xs-8 col-sm-6 col-xs-offset-2 col-sm-offset-3"><br>
                         <!-- Trigger Modal -->
                         <button type="button" class="btn green-jungle btn-block btn-lg pull-right" id="send-client">Send to Client</button><br><!-- Trigger Modal -->
                     </div><!-- End Form -->
@@ -156,9 +156,9 @@
                 </div>
               </div>
             </div>
-          </div> 
-          
-          <!-- Modal Container -->            
+          </div>
+
+          <!-- Modal Container -->
             <div class="row" id="result-step-2" style="display:none">
               <div class="col-xs-12 text-center">
                 <p style="font-size: 126px;"><i class="fa fa-check-circle" style="color: #5cb85c;background-color: #fff;"></i></p>
@@ -174,15 +174,15 @@
   </div><!-- Modal Fade Close-->
 
 
-  <!-- NRE Modal Start -->    
+  <!-- NRE Modal Start -->
 
-<!-- Confirm Eligibility Modal -->                                        
+<!-- Confirm Eligibility Modal -->
     <div class="modal fade in bs-modal-lg" id="sendNRE" role="dialog" >
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-              <div class="modal-header">                  
+              <div class="modal-header">
                   <h4 class="modal-title">Send Info Email</h4>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>                  
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               </div>
               <div class="modal-body">
                   <div class="container-fluid">
@@ -201,15 +201,15 @@
     <script src="/js/referral-result-page.js?id={{ str_random(6) }}" type="text/javascript"></script>
 
 
-    <script src="/js/no-reply-emails.js?id={{ str_random(6) }}" type="text/javascript"></script>    
+    <script src="/js/no-reply-emails.js?id={{ str_random(6) }}" type="text/javascript"></script>
     <script src="/assets/global/plugins/jquery-repeater/jquery.repeater.js" type="text/javascript"></script>
     <script src="/assets/pages/scripts/form-repeater.min.js" type="text/javascript"></script>
 
     <script async defer>
         {!! $google_maps_script !!}
-    </script>  
+    </script>
     <script src="/js/panel-lawyers.js?id={{ str_random(6) }}" type="text/javascript"></script>
 @endsection
 
-@section('inline-scripts')  
+@section('inline-scripts')
 @endsection
