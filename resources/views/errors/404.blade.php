39e6ea3aad182716c9a9fcf87229b2ebe73a84dc
Sebastian Currea <sebastian.currea@gmail.com>
2018-12-27 10:49:44 +1100

    change message page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,9 +4,10 @@
 
 	<div class="note note-danger">
 
-	    <h4 class="block"> <strong>Error  <small>{{ $exception->getStatusCode() }}</small></strong> </h4>
-	    
-	    @if( isset($_GET['debug']) )
+		<h4 class="block"> <strong>Error  <small>{{ $exception->getStatusCode() }}</small></strong> </h4>
+		<p><h4>Page not found</h4></p>
+		<p>If you need help, please contact an admin <a href="mailto:{{ config('app.team_email') }}">{{ config('app.team_email') }}</a></p>
+		@if( isset($_GET['debug']) )
 		<p> <strong>{{ $exception->getFile() }} in line {{ $exception->getLine() }}</strong> </p>
 		<br>
 		<p>  {{ json_encode($exception->getTrace()) }} </p>
