b359d3bca19c4e8b7299e9abbe3b96da179364c2
Sebastian Currea <sebastian.currea@gmail.com>
2019-01-21 16:13:53 +1100

    Store user office id (service provider) in booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -164,6 +164,7 @@ class BookingController extends Controller
                 'ContactInstructions' => (is_null( request('reContact') ) ? '' : request('reContact') ),//reContact
                 'RemindNow' => (is_null( request('RemindNow') ) ? 0 : ( request('RemindNow') == 'Yes' ? 1 : 0 ) ),//phonepermission
                 'CreatedBy' => (is_null( request('created_by') ) ? '':request('created_by')),
+                'OfficeId'  => \App\Http\helpers::getUSerServiceProviderId()  // The user office (sp)
             ];
             // Create the booking object
             $booking = [
