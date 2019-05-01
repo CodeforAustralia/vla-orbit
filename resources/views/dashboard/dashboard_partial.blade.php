c2532aaa0a2b23617b77099c9e8a13e8414053fe
Christian Arevalo <carevaloq87@hotmail.com>
2019-05-01 10:19:51 +1000

    Style dashboard date

--- resources/views/dashboard/dashboard_partial.blade.php
+++ resources/views/dashboard/dashboard_partial.blade.php
@@ -1,11 +1,11 @@
-    <div class="note note-info" id="{{ $dashboard->id }}">
-    	@if( in_array( \App\Http\helpers::getRole(), ['Administrator']) )
-    	<a href="/dashboard/delete/{{ $dashboard->id }}" class="btn btn-xs red pull-right delete-content"> Delete </a>
-    	<a href="/dashboard/show/{{ $dashboard->id }}" class="btn btn-xs green pull-right"> Edit </a> 
-    	@endif
+    <div class="note note-info padding-bottom-20" id="{{ $dashboard->id }}">
+        @if( in_array( \App\Http\helpers::getRole(), ['Administrator']) )
+        <a href="/dashboard/delete/{{ $dashboard->id }}" class="btn btn-xs red pull-right delete-content"> Delete </a>
+        <a href="/dashboard/show/{{ $dashboard->id }}" class="btn btn-xs green pull-right"> Edit </a> 
+        @endif
         <h4 class="block bold font-grey-mint note-title">{{ $dashboard->title }}</h4>
         <p class=""> {!! $dashboard->body !!} </p>
         @if ( \App\Http\helpers::getRole() == 'Administrator' )
-        <p class="dashboard_date"> <small>{!! date('d/m/Y', strtotime($dashboard->created_at)) !!} </small> </p>
+        <p class="dashboard_date pull-right"> <small>{!! date('d/m/Y', strtotime($dashboard->created_at)) !!} </small> </p>
         @endif
     </div>
