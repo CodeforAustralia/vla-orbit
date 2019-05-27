0c92265eb134bd7375e61b6b2ea2c53d6d1cdd91
Christian Arevalo <carevaloq87@hotmail.com>
2019-05-27 16:22:30 +1000

    Add tab navigation option to service edit/creation

--- resources/views/service/self-serving/form.blade.php
+++ resources/views/service/self-serving/form.blade.php
@@ -1,13 +1,48 @@
 <div class="service-container">
 
-    <service-general-settings></service-general-settings>
+    <div class="portlet box yellow">
+        <div class="portlet-title">
+            <div class="caption">
+                <i class="fa fa-gift"></i>Service
+            </div>
+        </div>
+        <div class="portlet-body" style="display: block;">
+            <div class="tabbable-line">
+                <ul class="nav nav-tabs ">
+                    <li class="active">
+                        <a href="#service_general_settings" data-toggle="tab"> General Settings </a>
+                    </li>
+                    <li>
+                        <a href="#service_clients_matters" data-toggle="tab"> Client / Matters </a>
+                    </li>
+                    <li>
+                        <a href="#service_legal_matters" data-toggle="tab"> Legal Matters </a>
+                    </li>
+                    <li>
+                        <a href="#service_intake_options" data-toggle="tab"> Intake Options </a>
+                    </li>
+                </ul>
+                <div class="tab-content">
 
-    <service-clients-matters></service-clients-matters>
+                    <div class="tab-pane active" id="service_general_settings">
+                        <service-general-settings></service-general-settings>
+                    </div>
 
-    <service-legal-matters></service-legal-matters>
+                    <div class="tab-pane" id="service_clients_matters">
+                        <service-clients-matters></service-clients-matters>
+                    </div>
 
-    <service-intake-options></service-intake-options>
+                    <div class="tab-pane" id="service_legal_matters">
+                        <service-legal-matters></service-legal-matters>
+                    </div>
+                    <div class="tab-pane" id="service_intake_options">
+                        <service-intake-options></service-intake-options>
+                    </div>
 
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
