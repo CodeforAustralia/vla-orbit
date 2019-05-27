cb5352b8e670a55f59eb2bb8c9182cccd992bb16
Christian Arevalo <carevaloq87@hotmail.com>
2019-05-27 14:27:01 +1000

    create form / page skeleton

---
+++ resources/views/service/self-serving/form.blade.php
@@ -0,0 +1,16 @@
+<div class="service-container">
+
+    <service-general-settings></service-general-settings>
+
+    <service-clients-matters></service-clients-matters>
+
+    <service-legal-matters></service-legal-matters>
+
+    <service-intake-options></service-intake-options>
+
+</div>
+
+
+@section('scripts')
+    <script src="/js/service_management.js"></script>
+@endsection
