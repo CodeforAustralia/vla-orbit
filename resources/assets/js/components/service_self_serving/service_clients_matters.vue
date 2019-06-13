06c5edfdf2130f618e01cfb225df821986ffa995
Christian Arevalo <carevaloq87@hotmail.com>
2019-06-13 16:27:56 +1000

    Move booking questions to last step

--- resources/assets/js/components/service_self_serving/service_clients_matters.vue
+++ resources/assets/js/components/service_self_serving/service_clients_matters.vue
@@ -36,33 +36,6 @@
             </div>
             <!-- End: Eligibility Questions -->
 
-            <!-- Booking Questions -->
-            <div class="form-group">
-                <div class="col-xs-12">
-                    <p class="caption-subject font-purple-soft bold uppercase margin-bottom-0">Booking Questions</p>
-                </div>
-            </div>
-
-            <div class="form-group col-sm-12" v-for="question in service_booking_questions" :key='question.QuestionId'>
-                <div class="col-sm-4 col-md-3">
-                    <label class="pull-right" v-if="question.QuestionName != ''">
-                        {{question.QuestionName}}
-                    </label>
-                    <label class="pull-right" v-else>
-                        {{question.QuestionLabel}}
-                    </label>
-                </div>
-                <div class="col-sm-3 col-md-2">
-                    <select  class="form-control" v-model="question.Operator">
-                        <option v-for="operator in operators" :key="operator.value" v-bind:value="operator.value"> {{ operator.label }}</option>
-                    </select>
-                </div>
-                <div class="col-sm-5 col-md-3">
-                    <input type="text" class="form-control" v-model="question.QuestionValue" v-if="question.QuestionTypeName == 'multiple'" data-role=tagsinput>
-                    <input type="text" class="form-control" v-model="question.QuestionValue" v-else>
-                </div>
-            </div>
-            <!-- End: Booking Questions -->
 
             <div class="form-group">
                 <div class="col-sm-12">
@@ -94,27 +67,13 @@
             },
             current_service: {
                 default: function () { return {} }
-            },
-            service_booking_questions: {
-                type: Array,
-                default: function () { return [] }
             }
         },
         data () {
             return {
                 selected_questions: [], //Those are vulnerability/eligibility questions
-                selected_booking_questions: [],
                 selected_operator: [],
-                initial_client_matters: {},
-                operators: [
-                                { label: '>',value: '>' },
-                                { label: '>=', value: '>=' },
-                                { label: '<', value: '<' },
-                                { label: '<=', value: '<=' },
-                                { label: 'Equal', value: '=' },
-                                { label: 'Not Equal', value: '!=' },
-                                { label: 'IN', value: 'in' }
-                            ]
+                initial_client_matters: {}
             };
         },
         methods: {
@@ -129,44 +88,13 @@
                     return false;
                 });
             },
-            pre_select_booking_questions() {
-                let self = this;
-                let cs_booking_questions = self.current_service.ServiceBookingQuestions;
-                self.service_booking_questions.map( sb_question => {
-                    sb_question.Operator = '';
-                    sb_question.QuestionValue = '';
-                    cs_booking_questions.map( cs_question => {
-                        if(sb_question.QuestionId === cs_question.QuestionId) {
-                            sb_question.Operator = cs_question.Operator;
-                            sb_question.QuestionValue = cs_question.QuestionValue;
-                        }
-                    });
-                });
-            },
             set_initial_client_matters() {
                 this.initial_client_matters = Object.assign({}, this.get_client_matters());
-            },
-            get_booking_questions() {
-                let self = this;
-                let booking_questions = [];
-                let cs_booking_questions = self.service_booking_questions;
-
-                cs_booking_questions.map( cs_question => {
-                    if(cs_question.Operator != '' && cs_question.QuestionValue != '') {
-                        booking_questions.push({
-                            'QuestionId': cs_question.QuestionId,
-                            'Operator': cs_question.Operator,
-                            'QuestionValue': cs_question.QuestionValue,
-                        });
-                    }
-                });
-                return booking_questions;
             },
             get_client_matters(){
                 return {
                     sv_id: this.current_service.ServiceId,
-                    vulnerability: this.selected_questions.map(item => item.QuestionId),
-                    booking_question: this.get_booking_questions()
+                    vulnerability: this.selected_questions.map(item => item.QuestionId)
                 };
             },
             save_client_matters() {
@@ -209,7 +137,6 @@
         },
         created() {
             this.pre_select_questions();
-            this.pre_select_booking_questions();
             this.set_initial_client_matters();
             this.event_on_change_tab();
         },
