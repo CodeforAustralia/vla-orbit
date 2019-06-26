6a0f3e30165f1b8f1c723d07311931cd8355e029
Christian Arevalo <carevaloq87@hotmail.com>
2019-06-26 10:58:31 +1000

    Add funciton to search by LGA (not enabled)

--- public/js/search.js
+++ public/js/search.js
@@ -10,6 +10,7 @@ var searchPage = function()
 				data: data,
 				width: '100%',
 				placeholder: "Location ( suburb or postcode )",
+				//matcher: matchLocation,
 				selectOnClose: true
 			});
 		}).done(function() {
@@ -57,8 +58,7 @@ var searchPage = function()
 
 	var nextPage = function ()
 	{
-		$( "#next-page" ).on( "click", function(e)
-		{
+		$( "#next-page" ).on( "click", function(e) {
 			e.preventDefault();
 
 			var legal_issue = $('.legal_issue #single').select2('data');
@@ -156,6 +156,41 @@ var searchPage = function()
 		$.each(data.children, function (idx, child) {
 		//Compare against matter name and tags inside matters
 			if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 || child.Tag.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 ) {
+				filteredChildren.push(child);
+			}
+		});
+
+		// If we matched any of the timezone group's children, then set the matched children on the group
+		// and return the group object
+		if (filteredChildren.length) {
+			var modifiedData = $.extend({}, data, true);
+			modifiedData.children = filteredChildren;
+
+			// You can return modified objects from here
+			// This includes matching the `children` how you want in nested data sets
+			return modifiedData;
+		}
+
+		// Return `null` if the term should not be displayed
+		return null;
+	}
+
+	var matchLocation = function (params, data) {
+		// If there are no search terms, return all of the data
+		if ($.trim(params.term) === '') {
+			return data;
+		}
+
+		// Skip if there is no 'children' property
+		if (typeof data.children === 'undefined') {
+			return null;
+		}
+
+		// `data.children` contains the actual options that we are matching against
+		var filteredChildren = [];
+		$.each(data.children, function (idx, child) {
+			//Compare against matter name and tags inside matters
+			if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 || data.text.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 ) {
 				filteredChildren.push(child);
 			}
 		});
