c140f623af1cb2a249c9044c2215c6fdcad3d393
Christian Arevalo <carevaloq87@hotmail.com>
2019-06-26 10:37:20 +1000

    Fix identation

--- public/js/search.js
+++ public/js/search.js
@@ -1,108 +1,90 @@
-var searchPage = function() 
+var searchPage = function()
 {
 
-	var getCatchments = function () 
+	var getCatchments = function ()
 	{
 		$("#contentLoading").modal("show");
-	    //Referral section
-	    $.get("/catchment/listFormated", function(data, status){
-	        $("#single-prepend-text").select2({
-	            data: data,
-	            width: '100%',
-	            placeholder: "Location ( suburb or postcode )",
-	            selectOnClose: true
-	        });
-	    }).done(function(){	    	
-	        $("#contentLoading").modal("hide");
-            setSelectedValues();
-	    });
+		//Referral section
+		$.get("/catchment/listFormated", function(data, status){
+			$("#single-prepend-text").select2({
+				data: data,
+				width: '100%',
+				placeholder: "Location ( suburb or postcode )",
+				selectOnClose: true
+			});
+		}).done(function(){
+			$("#contentLoading").modal("hide");
+			setSelectedValues();
+		});
 
 	};
-	var setLegalIssue = function () 
-	{		
-	    $.get("/matter/listFormatedTrimmed", function(data, status){
-	        $(".legal_issue #single").select2({
-	            data: data,
-	            width: '100%',
-	            placeholder: "Legal matter ( e.g. overdue fines, insurance )",
-	            matcher: matchCustom,
+	var setLegalIssue = function ()
+	{
+		$.get("/matter/listFormatedTrimmed", function(data, status){
+			$(".legal_issue #single").select2({
+				data: data,
+				width: '100%',
+				placeholder: "Legal matter ( e.g. overdue fines, insurance )",
+				matcher: matchCustom,
 				selectOnClose: true,
 				escapeMarkup: function (text) {
 					return text;
 				}
-	        });
-	    }).done(function(){
-	        $("#contentLoading").modal("hide");
-	    	setSelectedValues();
-	    });
+			});
+		}).done(function(){
+			$("#contentLoading").modal("hide");
+			setSelectedValues();
+		});
 	}
 
 	var fillFilters = function()
 	{
 		let filters = ''
-    	if ($('#referral_CLC').is(":checked"))
+		if ($('#referral_CLC').is(":checked"))
 		{
-		  filters += $('#referral_CLC').val() + ',';			  
+			filters += $('#referral_CLC').val() + ',';
 		}
-    	if ($('#referral_VLA').is(":checked"))
+		if ($('#referral_VLA').is(":checked"))
 		{
-		 filters +=  $('#referral_VLA').val()+ ',';
+		filters +=  $('#referral_VLA').val()+ ',';
 		}
-    	if ($('#referral_NLP').is(":checked"))
+		if ($('#referral_NLP').is(":checked"))
 		{
-		  filters += $('#referral_NLP').val()+ ',';		  
+			filters += $('#referral_NLP').val()+ ',';
 		}
-    	if ($('#referral_PP').is(":checked"))
+		if ($('#referral_PP').is(":checked"))
 		{
-		  filters += $('#referral_PP').val()+ ',';		  
-		}	
+			filters += $('#referral_PP').val()+ ',';
+		}
 		return filters;
 	}
 
-	var nextPage = function () 
+	var nextPage = function ()
 	{
-	    $( "#next-page" ).on( "click", function(e) 
-	    {
-	        e.preventDefault();        
+		$( "#next-page" ).on( "click", function(e)
+		{
+			e.preventDefault();
 
-	        var legal_issue = $('.legal_issue #single').select2('data');
-        	var catchment = $('.location #single-prepend-text').select2('data');        	
-        	// get checkbox value 
-        	let filters = fillFilters();
-        	if(filters == ''){
-        		swal('Alert','Please Select a Service Provider Type', "warning");
-        	}
-        	else if( legal_issue[0].id != '' && catchment[0].id != '')
-        	{   
-	        	/*
-	        	if( catchment[0].id == ''  ) {	            	
-	        		
-	        		swal({
-							  title: 'Location not provided, do you want to continue? (Can take longer)',
-							  text: "You can provide where the client lives, works or studies.",
-							  type: 'info',
-							  showCancelButton: true,
-							  confirmButtonColor: '#3085d6',
-							  cancelButtonColor: '#d33',
-							  confirmButtonText: 'Yes, continue'
-							}, function() {
-						      // Redirect the user						      
-							  redirectStep( legal_issue[0].id, 0 );
-						    });
-						    
-	        	} 
-	        	*/	        	
-			  	redirectStep( legal_issue[0].id, catchment[0].id, filters );
-	        }
-		    else
-		    {        	
-		        swal("Alert", "Please Select a Legal Matter and catchment", "warning");  
-		    }
-	    	
-	    });
+			var legal_issue = $('.legal_issue #single').select2('data');
+			var catchment = $('.location #single-prepend-text').select2('data');
+			// get checkbox value
+			let filters = fillFilters();
+			if(filters == ''){
+				swal('Alert','Please Select a Service Provider Type', "warning");
+			}
+			else if( legal_issue[0].id != '' && catchment[0].id != '')
+			{
+				redirectStep( legal_issue[0].id, catchment[0].id, filters );
+			}
+			else
+			{
+				swal("Alert", "Please Select a Legal Matter and catchment", "warning");
+			}
+
+		});
 	}
 
-	var setSelectedValues = function () 
+	var setSelectedValues = function ()
 	{
 		if( getUrlParameter('mt_id') != null )
 		{
@@ -145,10 +127,10 @@ var searchPage = function()
 			}
 			if(count == 4)
 			{
-				$('#referral_All').prop('checked', true).trigger('change');				
+				$('#referral_All').prop('checked', true).trigger('change');
 			}
 			else if($('#collapse').hasClass('collapse'))
-			{				
+			{
 				$('#collapse').addClass('in');
 			}
 
@@ -157,104 +139,104 @@ var searchPage = function()
 
 	//Function taken from https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
 	//var tech = getUrlParameter('ca_id');
-	var getUrlParameter = function (sParam) 
+	var getUrlParameter = function (sParam)
 	{
-	    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-	    sURLVariables = sPageURL.split('&'),
-	    sParameterName,
-	    i;
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i;
 
-	    for (i = 0; i < sURLVariables.length; i++) 
-	    {
-	        sParameterName = sURLVariables[i].split('=');
+		for (i = 0; i < sURLVariables.length; i++)
+		{
+			sParameterName = sURLVariables[i].split('=');
 
-	        if (sParameterName[0] === sParam) 
-	        {
-	            return sParameterName[1] === undefined ? true : sParameterName[1];
-	        }
-	    }
+			if (sParameterName[0] === sParam)
+			{
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
 	};
 
-	var matchCustom = function (params, data) 
-    {
-      	// If there are no search terms, return all of the data
-      	if ($.trim(params.term) === '') {
-      	  	return data;
-      	}
-
-        // Skip if there is no 'children' property
-        if (typeof data.children === 'undefined') {
-        	return null;
-        }
-
-        // `data.children` contains the actual options that we are matching against
-        var filteredChildren = [];
-        $.each(data.children, function (idx, child) {
-        //Compare against matter name and tags inside matters
-	        if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 || child.Tag.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 ) {
-	          filteredChildren.push(child);
-	        }
-        });
-
-      	// If we matched any of the timezone group's children, then set the matched children on the group
-      	// and return the group object
-        if (filteredChildren.length) {
-	        var modifiedData = $.extend({}, data, true);
-	        modifiedData.children = filteredChildren;
-
-	        // You can return modified objects from here
-	        // This includes matching the `children` how you want in nested data sets
-	        return modifiedData;
-        }
-
-        // Return `null` if the term should not be displayed
-        return null;
-    }
-
-	var redirectStep = function ( mt_id, ca_id, filters ) 
+	var matchCustom = function (params, data)
 	{
-		$("#contentLoading").modal("show");    	
-	    window.location.href = "/referral/create/details/?ca_id=" + ca_id + "&mt_id=" + mt_id + "&filters=" + filters;
+		// If there are no search terms, return all of the data
+		if ($.trim(params.term) === '') {
+			return data;
+		}
+
+		// Skip if there is no 'children' property
+		if (typeof data.children === 'undefined') {
+			return null;
+		}
+
+		// `data.children` contains the actual options that we are matching against
+		var filteredChildren = [];
+		$.each(data.children, function (idx, child) {
+		//Compare against matter name and tags inside matters
+			if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 || child.Tag.toUpperCase().indexOf(params.term.toUpperCase()) !== -1 ) {
+			filteredChildren.push(child);
+			}
+		});
+
+		// If we matched any of the timezone group's children, then set the matched children on the group
+		// and return the group object
+		if (filteredChildren.length) {
+			var modifiedData = $.extend({}, data, true);
+			modifiedData.children = filteredChildren;
+
+			// You can return modified objects from here
+			// This includes matching the `children` how you want in nested data sets
+			return modifiedData;
+		}
+
+		// Return `null` if the term should not be displayed
+		return null;
+	}
+
+	var redirectStep = function ( mt_id, ca_id, filters )
+	{
+		$("#contentLoading").modal("show");
+		window.location.href = "/referral/create/details/?ca_id=" + ca_id + "&mt_id=" + mt_id + "&filters=" + filters;
 	}
 	// Function taken from https://www.sanwebe.com/2014/01/how-to-select-all-deselect-checkboxes-jquery
 	var setServiceProviderSelect = function()
 	{
-				
-	
-	$("#select_all").change(function(){  
-	    $(".checkbox").prop('checked', $(this).prop("checked")); 
+
+
+	$("#select_all").change(function(){
+		$(".checkbox").prop('checked', $(this).prop("checked"));
 	});
 
-	
-	$('.checkbox').change(function(){ 
-	    
-	    if(false == $(this).prop("checked")){ 
-	        $("#select_all").prop('checked', false); 
-	    }
-	    
-	    if ($('.checkbox:checked').length == $('.checkbox').length ){
-	        $("#select_all").prop('checked', true);
-	    }
+
+	$('.checkbox').change(function(){
+
+		if(false == $(this).prop("checked")){
+			$("#select_all").prop('checked', false);
+		}
+
+		if ($('.checkbox:checked').length == $('.checkbox').length ){
+			$("#select_all").prop('checked', true);
+		}
 	});
-		
-	$("#select_all").prop('checked', true).trigger('change'); 
+
+	$("#select_all").prop('checked', true).trigger('change');
 
 	}
-	
-    return {
-        //main function to initiate the module
-        init: function () 
-        {
-            getCatchments();
-            setLegalIssue();
-            nextPage();
-            setServiceProviderSelect();           
-        }
 
-    };
+	return {
+		//main function to initiate the module
+		init: function ()
+		{
+			getCatchments();
+			setLegalIssue();
+			nextPage();
+			setServiceProviderSelect();
+		}
+
+	};
 }();
 
-jQuery(document).ready(function() 
+jQuery(document).ready(function()
 {
-    searchPage.init();
+	searchPage.init();
 });
