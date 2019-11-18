c584dc0099ceece521e4efedc80874017c3bf73e
Christian Arevalo <carevaloq87@hotmail.com>
2019-11-18 14:52:30 +1100

    Remove static method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -115,7 +115,7 @@ class UserController extends Controller
                 ->roles()
                 ->attach(Role::where('name', 'CLC')->first());
 
-            User::notifyRegistration($user);
+            $user->notifyRegistration($user);
             //Create the User
             return redirect('/registration')->with('success', 'Your request has been received, you will recive an email once your account is activated.');
         }
