eb3e94b36d5ab1de20f9a7b8f99f006c5c0e627c
Christian Arevalo <carevaloq87@hotmail.com>
2020-05-07 12:12:07 +1000

    Change modal title

--- resources/views/referral/create/booking.blade.php
+++ resources/views/referral/create/booking.blade.php
@@ -1,18 +1,18 @@
 
-  <!-- Modal Start -->     
+  <!-- Modal Start -->
   <div class="modal fade" id="SelectBooking" tabindex="-1" role="dialog" aria-labelledby="SelectBookingLabel" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="SelectBookingLabel">Bookings</h4>
+          <h4 class="modal-title" id="SelectBookingLabel">Bookings and e-referrals</h4>
         </div>
         <!-- Modal Body -->
         <div class="modal-body">
-          <div class="container-fluid">                
+          <div class="container-fluid">
                 @include("booking.form_vue")
           </div> <!-- Modal Container -->
-            
+
         </div> <!-- Modal Body Close-->
       </div><!-- Modal Content Close-->
     </div><!-- Modal Dialogue Close-->
